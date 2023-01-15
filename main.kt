51af7a0e75be89117e0739b7c9c0a458e8620afa
TazakiN <tazkia.nizami@gmail.com>
2023-01-15 15:27:36 +0700

    First idea
    ide awal dulu dan masih abstrak

---
+++ main.kt
@@ -0,0 +1,10 @@
+
+fun main() {
+    println("""CONTAINERS SHIPPING PROGRAMS
+untuk menjalankan program ini, cukup ketikkan angka pilihan anda.
+
+Apa yang ingin anda lakukan?
+1. unload barang
+2. load barang
+3. mengecek barang""")
+}
